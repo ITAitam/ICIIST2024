7649567d8362b07fdf425e66aa25a61b2453712e
ITAitam <160508198+ITAitam@users.noreply.github.com>
2024-02-19 21:12:36 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+  // window.addEventListener('scroll', function() {
+  //   var sidebar = document.getElementById('sidebar');
+  //   var topPos = sidebar.getBoundingClientRect().top;
+  //   if (topPos <= 0) {
+  //     sidebar.classList.add('fixed-sidebar');
+  //   } else {
+  //     sidebar.classList.remove('fixed-sidebar');
+  //   }
+  // });
+
+  
